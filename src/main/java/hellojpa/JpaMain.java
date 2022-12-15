65a2dfcd664c9982a9ded8bd20406a51f48a9f5e
ohmyoungjin <sisaudwls@naver.com>
2022-12-15 18:27:29 +0900

    20221215 :: ex1-hello-jpa 영속성 컨텍스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -7,6 +7,7 @@ import javax.persistence.Persistence;
 import java.util.List;
 
 public class JpaMain {
+
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence
                 .createEntityManagerFactory("hello");
@@ -17,25 +18,16 @@ public class JpaMain {
         //트랜잭션 시작
         tx.begin();
         try {
-            /* 저장
-                Member member = new Member();
-                member.setId(1L);
-                member.setName("HelloA");
-                em.persist(meㄴmber);
-            */
-            /*수정
-            Member findMember = em.find(Member.class, 1L);
-            //update 개념은 트랜잭션이 해당하는 객체에 대한 데이터를 가지고 있다가
-            //마지막 트랜잭션 커밋 시점에서 바뀐 데이터가 있으면 update 해준다.
-            findMember.setName("helloJPA");
-            */
-            //맴버 객체에 대한 값을 다 가져오게 돼있다.
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
-            for (Member member : result) {
-                System.out.println("member.name" + member.getName());
-            }
-            //커밋
+            Member member = em.find(Member.class, 1L);
+            Member member1 = em.find(Member.class,1L);
+            System.out.println(member == member1);
+            /* 저장 */
+            //비영속 상태
+//            em.persist(member);
+            //*영속 상태* DB에 저장되진 않는다.
+            System.out.println("BEFORE ====================");
+            System.out.println("AFTER =====================");
+            //***트랜잭션 commit 하는 시점에서 영속성 컨텍스트에 있는 값들이 DB에 들어가게 된다
             tx.commit();
         } catch (Exception e) {
                 tx.rollback();
