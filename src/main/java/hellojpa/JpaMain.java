3bfbb06c58ead03e4d8084994012083e53c6edd8
ohmyoungjin <sisaudwls@naver.com>
2022-12-17 19:17:06 +0900

    20221217 :: ex1-hello-jpa mapping annotation

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,15 +18,6 @@ public class JpaMain {
         //트랜잭션 시작
         tx.begin();
         try {
-            em.persist(new Member(1L, "A"));
-            Member member1 = em.find(Member.class,1L);
-            System.out.println(member1);
-            /* 저장 */
-            //비영속 상태
-//            em.persist(member);
-            //*영속 상태* DB에 저장되진 않는다.
-            System.out.println("BEFORE ====================");
-            System.out.println("AFTER =====================");
             //***트랜잭션 commit 하는 시점에서 영속성 컨텍스트에 있는 값들이 DB에 들어가게 된다
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,7 +1,7 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Member {
@@ -9,29 +9,23 @@ public class Member {
     public Member() {
     }
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
     @Id
     private Long id;
-    private String name;
 
-    private int age;
-    public Long getId() {
-        return id;
-    }
+    @Column(name = "name")
+    private String username;
 
-    public String getName() {
-        return name;
-    }
+    private Integer age;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
 }
